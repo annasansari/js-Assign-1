cc846b4dcccebc7c2e34724b55c2b72b70d8f312
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-01-26 13:20:21 -0800

    Add files via upload

---
+++ JS Assignment # 1/app.js
@@ -0,0 +1,18 @@
+//Question # 1
+// alert("Hello");
+
+
+
+// /Question # 2
+// alert("Error! Please Enter a Correct Password")
+
+// Question # 3
+// alert("Welcome to JS Land \n Happy Coding..")
+
+
+// Question # 4
+// alert("Welcome to JS Land")
+// alert("Happy Coding..")
+
+// Question # 5 
+// done in web browser console
